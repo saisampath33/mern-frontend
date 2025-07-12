d92da3123d49d736b10a828e0a98a7e292953fc6
saisampath33 <sampathmay10@gmail.com>
2025-07-12 18:05:10 +0530

    update

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,81 +1,101 @@
-import "./Register.css"
-import { useState } from "react";
-import axios from "axios";
+import "./Register.css";
 // import { useRef } from "react";
-// export default function Register() {
-//     const firstName = useRef();
-//     const lastName = useRef();
-//     const email = useRef();
-//     const password = useRef();
-
-// const handleSubmit = () =>{
-//     const user ={
-//         firstName:firstName.current.value,
-//         lastName:lastName.current.value,
-//         email:email.current.value,
-//         Password:password.current.value
-//     }
-//     console.log(user)
-// }
-// return (
-// <div className="App-Register-Row">
-//     <div style={{backgroundColor:"white"}}>
-//     <h2>Register Page</h2>
-//     <p>
-//         <input type="text"  placeholder="Enter the FirstName: " ref={firstName} />
-//     </p>
-//     <p>
-//         <input type="text" placeholder="Enter the LastName: " ref={lastName} />
-//     </p>
-//     <p>
-//         <input type="text" placeholder="Enter the Email: " ref={email}/>
-//     </p>
-//     <p>
-//         <input type="password" placeholder="Enter the Password: " ref={password}/>
-//     </p>
-//     <p><button onClick={handleSubmit}>Submit</button></p>
-//     </div>
-// </div>
-// );
-// }
-
-
-
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function Register() {
-const[user,setUser] = useState({});
-const [error,setError] = useState()
+const [user, setUser] = useState({});
+const [error, setError] = useState();
+const Navigate = useNavigate()
 const API_URL = import.meta.env.VITE_API_URL
-const handleSubmit = async () =>{
-    try{
+const handleSubmit = async () => {
+try {
     const url = `${API_URL}/api/users/register`;
-    // const url = "https://mern-backend-mauve-two.vercel.app/api/users/register";
-    const result =  await axios.post(url,user);
-    setError("Data Saved Successfully")
-    console.log(result);
-    }catch(err){
-        console.log(err);
-        setError("Something Went Wrong")
-    }
+    const result = await axios.post(url, user);
+    setError("Data saved successfully");
+    Navigate("/login")
+} catch (err) {
+    console.log(err);
+    setError("Something went wrong");
 }
+};
 return (
 <div className="App-Register-Row">
-    <div style={{backgroundColor:"white"}}>
-    <h2>Register Page</h2>
+    <div style={{ backgroundColor: "white" }}>
+    <h2>Registration Form</h2>
     {error}
     <p>
-        <input type="text" onChange={(e)=>setUser({ ...user,firstName:e.target.value })} placeholder="Enter the FirstName: " />
+        <input
+        type="text"
+        onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+        placeholder="Enter First Name"
+        />
     </p>
     <p>
-        <input type="text" onChange={(e)=>setUser({ ...user,lastName:e.target.value })} placeholder="Enter the LastName: " />
+        <input
+        type="text"
+        placeholder="Enter Last Name"
+        onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+        />
     </p>
     <p>
-        <input type="text" onChange={(e)=>setUser({ ...user,email:e.target.value })}placeholder="Enter the Email: " />
+        <input
+        type="text"
+        placeholder="Enter Email Address"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
     </p>
     <p>
-        <input type="password" onChange={(e)=>setUser({ ...user,password:e.target.value })} placeholder="Enter the Password: " />
+        <input
+        type="password"
+        placeholder="Enter Password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
     </p>
-    <p><button onClick={handleSubmit}>Submit</button></p>
+    <p>
+        <button onClick={handleSubmit}>Submit</button>
+    </p>
+    <hr />
+    <Link to="/login">Already a member? Login Here...</Link>
     </div>
 </div>
 );
 }
+
+// export default function Register() {
+//   const firstName = useRef();
+//   const lastName = useRef();
+//   const email = useRef();
+//   const password = useRef();
+//   const handleSubmit = () => {
+//     const user = {
+//       firstName: firstName.current.value,
+//       lastName: lastName.current.value,
+//       email: email.current.value,
+//       password: password.current.value,
+//     };
+//     console.log(user);
+//   };
+//   return (
+//     <div className="App-Register-Row">
+//       <div style={{ backgroundColor: "white" }}>
+//         <h2>Registration Form</h2>
+//         <p>
+//           <input type="text" placeholder="Enter First Name" ref={firstName} />
+//         </p>
+//         <p>
+//           <input type="text" placeholder="Enter Last Name" ref={lastName} />
+//         </p>
+//         <p>
+//           <input type="text" placeholder="Enter Email Address" ref={email} />
+//         </p>
+//         <p>
+//           <input type="password" placeholder="Enter Password" ref={password} />
+//         </p>
+//         <p>
+//           <button onClick={handleSubmit}>Submit</button>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Users() {
+  const [users,setUsers] = useState([]);
+  const [error,setError] = useState();
+  const API_URL = import.meta.env.VITE_API_URL
+  const fetchUsers = async () =>{
+    try{
+      setError("Loading...")
+      const url= `${API_URL}/api/users`;
+    const result = await axios.get(url);
+    setUsers(result.data);
+    setError()
+    }catch(err){
+      console.log(err);
+      setError("Something went Wrong");
+    }
+  };
+  useEffect(()=>{
+    fetchUsers()
+  },[])
   return (
     <div>
       Users
