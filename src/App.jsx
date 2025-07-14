5d74cfa1fe2f39777185b3889c946091d22082d3
saisampath33 <sampathmay10@gmail.com>
2025-07-14 23:24:42 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import Home from './components/Home.jsx'
-// import ScoreBoard from './components/ScoreBoard.jsx'
-import Register from './components/Register.jsx'
-import Product from './components/Product.jsx'
-import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
-import Admin from './components/Admin.jsx'
-import Users from './components/Users.jsx'
-import Orders from './components/Orders.jsx'
-import Login from './components/Login.jsx'
-import Products from './components/Products.jsx'
-import Cart from './components/Cart.jsx'
-import Order from './components/Order.jsx'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Register from "./components/Register";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Order from "./components/Order";
+import Admin from "./components/Admin";
+import Users from "./components/Users";
+import Orders from "./components/Orders";
+import Login from "./components/Login";
+import Products from "./components/Products";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
 function App() {
   return (
-    <div className='App-container'>
-<BrowserRouter>
-    <h1 style={{ backgroundColor:"orange"}}>MERN FRONTEND</h1>
-    <Link to="/">Home</Link>-
-    <Link to="/cart">My Cart</Link>-
-    <Link to="/order">My Orders</Link>-
-    <Link to="/admin">Admin</Link>-
-    <Link to="/login">Login</Link>
-    <Routes>
-      <Route index element={<Product/>}/>
-      <Route path="login" element={<Login/>}/>
-      <Route path="register" element={<Register/>}/>
-      <Route path="cart" element={<Cart/>}/>
-      <Route path="order" element={<Order/>}/>
-      <Route path="admin" element={<Admin/>}>
-        <Route index element={<Users/>}/>
-        <Route path="products" element={<Products/>}/>
-        <Route path="orders" element={<Orders/>}/>
-      </Route>
-    </Routes>
-    <h3>This is footer</h3>
-</BrowserRouter>
-
+    <div className="App-Container">
+      <BrowserRouter>
+        <h1>MERN Frontend</h1>
+        <Link to="/">Home</Link>-<Link to="/cart">MyCart</Link>-
+        <Link to="/order">MyOrder</Link>-<Link to="/admin">Admin</Link>-
+        <Link to="/login">Login</Link>
+        <Routes>
+          <Route index element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="order" element={<Order />} />
+          <Route path="admin" element={<Admin />}>
+            <Route index element={<Users />} />
+            <Route path="products" element={<Products />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
+        </Routes>
+        <h3>This is footer</h3>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
